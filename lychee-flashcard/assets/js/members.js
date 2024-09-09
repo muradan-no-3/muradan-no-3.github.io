5184b3f2d6c33531f39099edb573fe2b011ac075
muradandan <muradan.warmvillage@gmail.com>
2024-09-09 18:07:32 +0900

    member page home link, scroll and touch event

--- lychee-flashcard/assets/js/members.js
+++ lychee-flashcard/assets/js/members.js
@@ -189,4 +189,36 @@ window.addEventListener("DOMContentLoaded", () => {
 
     wrapper.appendChild(currentClass);
   });
+
+  document.addEventListener("touchstart", () => {
+    const target = document.querySelector(".js-fixed-size");
+    target.classList.add("onmoving");
+  });
+
+  document.addEventListener("touchend", () => {
+    const target = document.querySelector(".js-fixed-size");
+    target.classList.remove("onmoving");
+  });
+
+  document.addEventListener("scroll", () => {
+    fixTarget();
+  });
+  window.visualViewport.addEventListener("scroll", () => {
+    fixTarget();
+  });
+  window.visualViewport.addEventListener("resize", () => {
+    fixTarget();
+  });
+
+  function fixTarget() {
+    const offsetX = window.visualViewport.offsetLeft;
+    const offsetY = window.visualViewport.offsetTop;
+    const scale = window.visualViewport ? 1 / window.visualViewport.scale : 1;
+
+    console.log(offsetX, offsetY, scale);
+
+    const target = document.querySelector(".js-fixed-size");
+    target.style.scale = scale;
+    target.style.translate = Number(4 * scale + offsetX) + "px " + Number(16 * scale + offsetY) + "px";
+  }
 });
